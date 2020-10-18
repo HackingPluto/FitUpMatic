0264f5576468964aa9ec698be1ae2e7176f806be
PythonIsNotASnake <62463780+PythonIsNotASnake@users.noreply.github.com>
2020-10-18 17:23:25 +0200

    Add files via upload

---
+++ app/src/main/java/de/pint/fitupmatic/MainActivity.java
@@ -0,0 +1,14 @@
+package de.pint.fitupmatic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
